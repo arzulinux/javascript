f26b65e04845c233b9cc9fdb03760adb48842d0a
arzulinux <arysuyitno82@gmail.com>
2015-12-26 20:27:02 +0700

    Update sitemap2.js

--- sitemap2.js
+++ sitemap2.js
@@ -1,4 +1,7 @@
-
+/*
+ * RAccordion for TOC
+ * 
+ */
 eval(function (p, a, c, k, e, r) {
     e = function (c) {
         return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
